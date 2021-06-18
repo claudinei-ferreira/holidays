eeba84cc4ef22376054d5ca6eaf86e13421f6287
Claudinei <claufersus@gmail.com>
2021-06-18 20:52:56 -0300

    Refatorando Classe de Holidays

--- src/Holiday.php
+++ src/Holiday.php
@@ -54,7 +54,7 @@ class Holiday
     {
         foreach($holidays as $holiday){
             $date = "{$this->year}-{$holiday->date}";
-            $this->setHoliday(strtotime($date), $holiday->title);
+            $this->setHoliday(strtotime($date), $holiday->name);
         }    
     }
 
@@ -74,14 +74,14 @@ class Holiday
         $this->includeHolidays($holidays);
     }
 
-    private function setHoliday($timestamp, $description)
+    private function setHoliday($timestamp, $name)
     {
         if (date("n", $timestamp) == $this->month) {
 
             $this->holidays[] = [
                 'day' => str_pad(date('j', $timestamp), 2, 0, STR_PAD_LEFT),
                 'date' => date('Y-m-d', $timestamp),
-                'name' => $description,
+                'name' => $name,
             ];
 
         }
@@ -106,57 +106,58 @@ class Holiday
     private function loadHolidays()
     {
         $this->otherHolidays = [
-            ['date'  => '01-01','title' => 'Confraternização Universal'],
-            ['date' => '01-06','title' => 'Dia de Reis'],
-            ['date' => '01-07','title' => 'Dia do Leitor'],
-            ['date' => '01-31','title' => 'São João Bosco'],
-            ['date' => '03-08','title' => 'Dia da Mulher'],
-            ['date' => '03-12','title' => 'Dia do Bibliotecário'],
-            ['date' => '03-15','title' => 'Dia da Escola'],
-            ['date' => '03-19','title' => 'São José'],
-            ['date' => '04-01','title' => 'Canonização de Dom Bosco (1934)'],
-            ['date' => '04-09','title' => 'Dia da Biblioteca'],
-            ['date' => '04-18','title' => 'Dia de Monteiro Lobato'],
-            ['date' => '04-19','title' => 'Dia do Índio'],
-            ['date' => '04-21','title' => 'Tiradentes'],
-            ['date' => '04-25','title' => 'Dia do Contabilista'],
-            ['date' => '04-28','title' => 'Dia da Educação'],
-            ['date' => '05-01','title' => 'Dia do Trabalho (São José Operário, memória)'],
-            ['date' => '05-06','title' => 'São Domingos Sávio'],
-            ['date' => '05-08','title' => 'Dia do Profissional Marketing'],
-            ['date' => '05-13','title' => 'Santa Maria Domingas Mazzarello'],
-            ['date' => '05-24','title' => 'Nossa Senhora Auxiliadora'],
-            ['date' => '06-03','title' => 'Dia do Profissional de RH'],
-            ['date' => '06-13','title' => 'Dia de Santo Antônio'],
-            ['date' => '06-24','title' => 'Natividade de São João Batista' ],
-            ['date' => '06-29','title' => 'Dia de São Pedro' ],
-            ['date' => '06-29','title' => 'Dia de São Paulo' ],
-            ['date' => '07-09','title' => 'Revolução Constitucionalista de 1932' ],
-            ['date' => '07-14','title' => 'Chegada dos Salesianos no Brasil' ],
-            ['date' => '07-26','title' => 'Dia da Vovó' ],
-            ['date' => '07-27','title' => 'Dia dos Avós' ],
-            ['date' => '08-01','title' => 'Aniversário de Piracicaba'],
-            ['date' => '08-04','title' => 'Dia do Padre'],
-            ['date' => '08-11','title' => 'Dia do Estudante'],
-            ['date' => '08-16','title' => 'Aniversário de Dom Bosco'],
-            ['date' => '08-22','title' => 'Dia do Coordenador Pedagógico'],
-            ['date' => '09-07','title' => 'Dia da Independência'],
-            ['date' => '09-22','title' => 'Dia do Contador'],
-            ['date' => '10-12','title' => 'Nossa Senhora Aparecida'],
-            ['date' => '10-12','title' => 'Dia das Crianças'],
-            ['date' => '10-15','title' => 'Dia do Professor'],
-            ['date' => '10-19','title' => 'Dia do Profissional de TI'],
-            ['date' => '11-01','title' => 'Todos os Santos'],
-            ['date' => '11-02','title' => 'Finados'],
-            ['date' => '11-15','title' => 'Proclamação da República'],
-            ['date' => '11-19','title' => 'Dia da Bandeira'],
-            ['date' => '11-20','title' => 'Dia Da Consciência Negra'],
-            ['date' => '12-04','title' => 'Dia do Orientador Educacional'],
-            ['date' => '12-08','title' => 'Imaculada Conceição'],
-            ['date' => '12-24','title' => 'Véspera de Natal'],
-            ['date' => '12-25','title' => 'Natal'],
-            ['date' => '12-30','title' => 'Sagrada Família, Jesus, Maria e José'],
-            ['date' => '12-31','title' => 'Véspera de Ano Novo']
+            ['date'  => '01-01','name' => 'Confraternização Universal'],
+            ['date' => '01-06','name' => 'Dia de Reis'],
+            ['date' => '01-07','name' => 'Dia do Leitor'],
+            ['date' => '01-31','name' => 'São João Bosco'],
+            ['date' => '03-08','name' => 'Dia da Mulher'],
+            ['date' => '03-12','name' => 'Dia do Bibliotecário'],
+            ['date' => '03-15','name' => 'Dia da Escola'],
+            ['date' => '03-19','name' => 'São José'],
+            ['date' => '04-01','name' => 'Canonização de Dom Bosco (1934)'],
+            ['date' => '04-09','name' => 'Dia da Biblioteca'],
+            ['date' => '04-18','name' => 'Dia de Monteiro Lobato'],
+            ['date' => '04-19','name' => 'Dia do Índio'],
+            ['date' => '04-21','name' => 'Tiradentes'],
+            ['date' => '04-22','name' => 'Descobrimento do Brasil'],
+            ['date' => '04-25','name' => 'Dia do Contabilista'],
+            ['date' => '04-28','name' => 'Dia da Educação'],
+            ['date' => '05-01','name' => 'Dia do Trabalho (São José Operário, memória)'],
+            ['date' => '05-06','name' => 'São Domingos Sávio'],
+            ['date' => '05-08','name' => 'Dia do Profissional Marketing'],
+            ['date' => '05-13','name' => 'Santa Maria Domingas Mazzarello'],
+            ['date' => '05-24','name' => 'Nossa Senhora Auxiliadora'],
+            ['date' => '06-03','name' => 'Dia do Profissional de RH'],
+            ['date' => '06-13','name' => 'Dia de Santo Antônio'],
+            ['date' => '06-24','name' => 'Natividade de São João Batista' ],
+            ['date' => '06-29','name' => 'Dia de São Pedro' ],
+            ['date' => '06-29','name' => 'Dia de São Paulo' ],
+            ['date' => '07-09','name' => 'Revolução Constitucionalista de 1932' ],
+            ['date' => '07-14','name' => 'Chegada dos Salesianos no Brasil' ],
+            ['date' => '07-26','name' => 'Dia da Vovó' ],
+            ['date' => '07-27','name' => 'Dia dos Avós' ],
+            ['date' => '08-01','name' => 'Aniversário de Piracicaba'],
+            ['date' => '08-04','name' => 'Dia do Padre'],
+            ['date' => '08-11','name' => 'Dia do Estudante'],
+            ['date' => '08-16','name' => 'Aniversário de Dom Bosco'],
+            ['date' => '08-22','name' => 'Dia do Coordenador Pedagógico'],
+            ['date' => '09-07','name' => 'Dia da Independência'],
+            ['date' => '09-22','name' => 'Dia do Contador'],
+            ['date' => '10-12','name' => 'Nossa Senhora Aparecida'],
+            ['date' => '10-12','name' => 'Dia das Crianças'],
+            ['date' => '10-15','name' => 'Dia do Professor'],
+            ['date' => '10-19','name' => 'Dia do Profissional de TI'],
+            ['date' => '11-01','name' => 'Todos os Santos'],
+            ['date' => '11-02','name' => 'Finados'],
+            ['date' => '11-15','name' => 'Proclamação da República'],
+            ['date' => '11-19','name' => 'Dia da Bandeira'],
+            ['date' => '11-20','name' => 'Dia Da Consciência Negra'],
+            ['date' => '12-04','name' => 'Dia do Orientador Educacional'],
+            ['date' => '12-08','name' => 'Imaculada Conceição'],
+            ['date' => '12-24','name' => 'Véspera de Natal'],
+            ['date' => '12-25','name' => 'Natal'],
+            ['date' => '12-30','name' => 'Sagrada Família, Jesus, Maria e José'],
+            ['date' => '12-31','name' => 'Véspera de Ano Novo']
         ];
 
         return json_decode(json_encode((object) $this->otherHolidays), FALSE);
