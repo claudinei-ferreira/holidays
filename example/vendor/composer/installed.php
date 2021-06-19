081ac2e537915c784d669ec06542b7f3cd0301a6
Claudinei <claufersus@gmail.com>
2021-06-19 14:23:11 -0300

    Novas implementação no arquivo README

---
+++ example/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'ccfb3b6162cd419b4e89334f9732d364c99e7257',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ccfb3b6162cd419b4e89334f9732d364c99e7257',
+    ),
+    'claufersus/holidays' => 
+    array (
+      'pretty_version' => '0.0.9',
+      'version' => '0.0.9.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ccfb3b6162cd419b4e89334f9732d364c99e7257',
+    ),
+  ),
+);
